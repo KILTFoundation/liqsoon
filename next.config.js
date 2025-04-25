436aacca32669751c275bd239641210fa51fd4b3
KILTFoundation <svet@kilt.io>
2025-04-25 09:47:46 +0100

    Update next.config.js
    added sepolia

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
               "script-src 'self' 'unsafe-inline'; " +
               "style-src 'self' 'unsafe-inline'; " +
               "img-src 'self' data:; " +
-              "connect-src 'self' https://*.thirdweb.com wss://*.thirdweb.com https://rpc.base.org; " +
+              "connect-src 'self' https://*.thirdweb.com wss://*.thirdweb.com https://rpc.base.org https://sepolia.base.org; " +
               "frame-src 'self' https://*.thirdweb.com;",
           },
         ],
